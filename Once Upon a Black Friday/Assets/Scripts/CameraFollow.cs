1c70adfbee7bd764fe09635c9fa70dc79413268a
MichaelTaylor <michael.taylor.gamedev@gmail.com>
2016-08-30 14:19:14 -0500

    [General] added a to do list as far as scripting

---
+++ Once Upon a Black Friday/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject Player;
+
+	Vector3 Offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Offset = transform.position - Player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = Player.transform.position + Offset;
+	}
+}
--- Once Upon a Black Friday/Assets/Scripts/PlayerController.cs
+++ Once Upon a Black Friday/Assets/Scripts/PlayerController.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	public float speed, RageMeter;
+	public LayerMask Hittable;
+	public GameObject [] WeaponsOnHand;
 
 	Rigidbody2D RB2D;
-	public LayerMask Hittable;
 
 	// Use this for initialization
 	void Start () 
 	{
+		WeaponsOnHand = new GameObject[3];
 		RB2D = GetComponent<Rigidbody2D> ();
 	}
 	
@@ -23,18 +25,49 @@ public class PlayerController : MonoBehaviour {
 
 		Vector2 movement = new Vector2 (horizontal, vertical);
 		RB2D.velocity = movement * speed;
+		Sprinting ();
 
 		//MAKES THE PLAYER LOOK AT THE MOUSE
 		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
 
 		//RAYCAST FOR WEAPON DISTANCE
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePos, 1f, Hittable);
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePos, 0.3f, Hittable);
 		Debug.DrawRay(transform.position, mousePos, Color.red);
 
 		if (hit.collider != null) 
 		{
 			Debug.Log ("Hit Something");
 		}
+	}
+
+	void Sprinting()
+	{
+		if (Input.GetButton ("Sprint")) 
+		{
+			speed = 2;
+		} 
+		else 
+		{
+			speed = 1;
+		}
+	}
+
+	void SwitchWeapons()
+	{
+		if (Input.GetAxis ("Mouse ScrollWheel") != 0) 
+		{
+			Debug.Log ("Switch");
+		}
+	}
+
+	void ThrowAwayWeapon()
+	{
+		//TODO: add force to weapons
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//TODO: Add weapon pickup and functionality
 	}
 }
